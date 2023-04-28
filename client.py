fdeb025934931014816b3769d80b01d3abc2db50
Diana Yue <dandan@dhcp-10-250-40-210.harvard.edu>
2023-04-27 21:58:56 -0400

    finalized feature where of client generating image and sending to all clients

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import argparse
 import sys
 import select
+import threading
 
 class Client:
     def __init__(self) -> None:
@@ -13,13 +14,11 @@ class Client:
         self.clientsocket.connect((host, port))
 
     # check for error in command line prompt
-    def check_error_command(self, request_type: str, request: str):
+    def check_error_command(self, request: str):
         request = request.split()
         errno = 0
-        commands = ["gen"]
-        if len(request) == 0 or request[0] not in commands:
+        if len(request) == 0:
             errno = 1
-
         elif request[0] == "gen":
             if len(request) < 2:
                 errno = 1
@@ -30,31 +29,36 @@ class Client:
         self.clientsocket.send(request.encode())
 
     def receive_response(self):
-        response = self.clientsocket.recv(1024).decode()
-        return response
-
-    def run(self, request: str):
         while True:
-            error_code = self.check_error_command("gen", request)
-            if error_code != 0:
-                print("Error: Invalid input. Please provide a valid command.")
-                
-            elif request == "exit": # Exit the loop and close the client socket
+            response = self.clientsocket.recv(1024).decode()
+            print("Image URL: ", response)
+            return response
+
+    def run(self):
+        listen_thread = threading.Thread(target=self.receive_response)
+        listen_thread.start()
+
+        while True:
+            request = input("Enter a command (gen <parameter>, 'quit' to quit): ")
+            
+            # Exit the loop and close the client socket
+            if request == "quit": 
+                self.send_request("quit")
                 self.clientsocket.close()
                 print("Client closed.")
                 break
-            else:
+            elif self.check_error_command(request) == 0:
                 self.send_request(request)
-                response = self.receive_response()
-                print("Image URL: ", response)
+            else:
+                print("Error: Invalid input. Please provide a valid command.")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Send a request to the server to generate an image.')
-    parser.add_argument('request', type=str, help='The request to send to the server, e.g., "gen white cat".')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='Send a request to the server to generate an image.')
+    # parser.add_argument('request', type=str, help='The request to send to the server, e.g., "gen white cat".')
+    # args = parser.parse_args()
 
     client = Client()
-    client.run(args.request)
+    client.run()
     
 
