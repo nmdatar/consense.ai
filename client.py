65bae0ca636382b257dd3f19f6089f1bb4c82307
Nikhil Datar <ndatar18@gmail.com>
2023-05-03 09:12:50 -0400

    Replication 1) heartbeat, 2) update connections, 3) await incoming connections, 4) takeover as master

--- client.py
+++ client.py
@@ -3,15 +3,15 @@ import argparse
 import sys
 import select
 import threading
+from rich.console import Console
 
 class Client:
-    def __init__(self) -> None:
+    def __init__(self, host: str, port: int) -> None:
         #Create socket object
         self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host = 'localhost'
-        port = 8000
-        # Connect to the server
-        self.clientsocket.connect((host, port))
+        self.host = host
+        self.port = port
+
 
     # check for error in command line prompt
     def check_error_command(self, request: str):
@@ -21,10 +21,12 @@ class Client:
             errno = 1
         elif request[0] == "gen":
             if len(request) < 2:
+                errno = 1
         elif request[0] == "vote":
             if len(request) != 2 or request[1] not in ["Y", "N"]:
                 errno = 1
         
+
         return errno
 
     def send_request(self, request: str):
@@ -32,18 +34,40 @@ class Client:
 
     def receive_response(self):
         while True:
-            response = self.clientsocket.recv(1024).decode()
-            print("Image URL: ", response)
-            vote = input("Vote for this image? (Y/N): ")
-            self.send_request(f"vote {vote}")
-            return response
 
-    def run(self):
+            try: 
+                response = self.clientsocket.recv(1024)
+                if response:
+                    response = response.decode()
+                    print(response)
+                    print('> ')
+                else:
+                    self.clientsocket.close()
+                    
+            except Exception as e:
+                break 
+
+
+    def run(self) -> None:
+
+        try: 
+            # Connect to the server
+            self.clientsocket.connect((self.host, self.port))
+
+        except Exception:
+            return 
+        
         listen_thread = threading.Thread(target=self.receive_response)
         listen_thread.start()
 
         while True:
-            request = input("Enter a command (gen <parameter>, 'vote Y', 'vote N', 'quit' to quit): ")
+            request = input("> ")
+
+            # Check if listen_thread has ended
+            if not listen_thread.is_alive():
+                print("Server closed the connection.")
+                # self.clientsocket.close()
+                return 
             
             # Exit the loop and close the client socket
             if request == "quit": 
@@ -58,11 +82,15 @@ class Client:
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description='Send a request to the server to generate an image.')
-    # parser.add_argument('request', type=str, help='The request to send to the server, e.g., "gen white cat".')
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Client Info')
+    parser.add_argument('--hosts', nargs='+', type=str, help='IPV4 Host')
+    parser.add_argument('--ports', nargs='+', type=int, help='Port for connection')
+    args = parser.parse_args()
 
-    client = Client()
-    client.run()
+    for host, port in zip(args.hosts, args.ports):
+        print('Connecting to new server: ', host, port)
+        client = Client(host=host, port=port)
+        client.run()
     
+
 
--- server.py
+++ server.py
@@ -4,26 +4,153 @@ import os
 import argparse
 from typing import List, Dict, Tuple
 import requests
-import ipfshttpclient
+# import ipfshttpclient
 import socket
 import threading
+import json
+import time
 
 load_dotenv()
 
 class Server:
-    def __init__(self, host: str, port: int = 8000, id: int = 1) -> None:
+    def __init__(self, host: str, port: int = 8000, id: int = 1, primary: bool = True, primary_host : str = 'localhost', primary_port : int = 8003) -> None:
         self.host = host
         self.port = port
         self.id = id
         openai.api_key = os.getenv('OPENAI_API_KEY')
         self.API_KEY = openai.api_key
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((host, port))
+        self.server.bind((self.host, self.port))
         self.server.listen(5)
         self.clients = {}
         self.generate_available = 0
-        self.image_votes = {} #keep track of votes per image
-        print(f"Server started on {host}:{port}")
+        self.image_votes = {}
+        self.primary = primary
+        self.internal_port = self.port + 1
+        
+        self.receive_port = self.port + 2
+        self.receive = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.await_port = self.port + 3
+        self.await_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.await_socket.bind((self.host, self.await_port))
+        self.await_socket.listen(5)
+        self.receive.bind((self.host, self.receive_port))
+        self.receive.listen(5)
+
+        # Change primary_host etc if it is a secondary server
+        if not self.primary:
+            self.primary_host = primary_host
+            self.primary_port = primary_port
+        else:
+            self.primary_host = self.host
+            self.primary_port = self.await_port
+
+        self.backup_servers = {}
+        
+        print(f"Server started on {self.host}:{self.port}")
+
+    def maintain_heartbeat_socket(self) -> None:
+        self.internal = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.internal.bind((self.host, self.internal_port))
+        self.internal.listen(5)
+
+        while True:
+            heartbeat_server, addr = self.internal.accept()
+            heartbeat_server.close()
+
+    def await_servers(self) -> None:
+        while True:
+            try: 
+                new_server, addr = self.await_socket.accept()
+                data = b''
+                while True:
+                    
+                    chunk = new_server.recv(1024)
+                    if not chunk:
+                        break
+                    data += chunk
+                print('Received all data from connecting server')
+                data = data.decode()
+                msg = json.loads(data)
+                id = msg.pop('id')
+                self.backup_servers[id] = msg
+                self.update_backups()
+
+            except Exception:
+                break
+        
+
+    def update_backups(self) -> None:
+        msg = {}
+        if self.id in self.backup_servers.keys():
+            self.backup_servers.pop(self.id)
+        msg['backup_servers'] = self.backup_servers
+        msg['primary_host'] = self.host
+        msg['primary_port'] = self.internal_port
+        msg = json.dumps(msg)
+        
+        for backup in self.backup_servers.values():
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                host = backup['host']
+                port = backup['receive_port']
+                s.connect((host, port))
+                s.sendall(msg.encode())
+                s.close()
+
+    def receive_updates(self) -> None:
+        while not self.primary:
+            
+            try:
+                main_server, addr = self.receive.accept()
+                
+                data = b''
+                while True:
+                    chunk = main_server.recv(1024)
+                    if not chunk:
+                        break
+                    data += chunk
+                
+                data = data.decode()
+                msg = json.loads(data)
+                
+                self.primary_host = msg['primary_host']
+                self.primary_port = msg['primary_port']
+                self.backup_servers = msg['backup_servers']
+                
+            except Exception:
+                break
+
+        return 
+    
+    def heartbeat(self) -> None:
+        while True:
+            if not self.primary:
+                try:
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                        s.settimeout(1)
+                        s.connect((self.primary_host, self.primary_port))
+                        s.close()
+                except Exception as e:
+
+                    if self.id == int(min(self.backup_servers.keys())):
+                        print("Taking Over ;)")
+                        self.backup_servers.pop(str(self.id))
+                        self.primary = True
+                        self.primary_host = self.host
+                        self.primary_port = self.internal_port
+                        self.update_backups()
+                        await_thread = threading.Thread(target=self.await_servers)
+                        await_thread.start()
+                        maintain_heartbeat_thread = threading.Thread(target=self.maintain_heartbeat_socket)
+                        maintain_heartbeat_thread.start()
+                        return 
+                        
+                    else:
+                        print('Connected to new master: ', self.primary_host, self.primary_port)
+                        time.sleep(2)
+
+            time.sleep(2)
+        
     
     def generate_image(self, prompt: str) -> Dict:
         response = openai.Image.create(
@@ -33,21 +160,19 @@ class Server:
         )
         return response
 
-    def send_image(self, prompt, client):
-        print(self.generate_available)
+    def send_image(self, prompt, client) -> None:
         try:
             self.generate_available = 1
-            image_response = self.generate_image(prompt)
+            image_response = self.generate_image(prompt=prompt)
             image_url = image_response['data'][0]['url']
             print("Generated image URL:", image_url)
             self.send_to_all_clients(image_url)
-            self.image_votes[image_url] = {"Y": 0, "N": 0} #initialize votes
             print(f"shared image to", [client.getpeername() for client in self.clients])
         except Exception as e:
             print("Exception:", e)
             self.send_to_all_clients(f"Error: {e}")
 
-    def send_to_all_clients(self, message: str):
+    def send_to_all_clients(self, message: str) -> None:
         for client in self.clients:
             try:
                 client.send(message.encode()) 
@@ -56,7 +181,7 @@ class Server:
                 client.close()
                 self.clients.pop(client)
 
-    def handle_client(self, client):
+    def handle_client(self, client) -> None:
         addr = client.getpeername()
         self.clients[client] = "active"
         print("waiting for responses")
@@ -73,27 +198,57 @@ class Server:
                 prompt = request
                 self.send_image(prompt, client)
                 print(f"Received prompt: {prompt}")
-            elif request[:4] == "vote": 
-                vote = request.split()[1]
-                image_url = self.clients[client]
-                if image_url in self.image_votes:
-                    self.image_votes[image_url][vote] += 1
-                    self.send_to_all_clients(f"Vote updated for {image_url}: {self.image_votes[image_url]}")
-                    print(f"Image {image_url} vote updated: {self.image_votes[image_url]}")
-                else:
-                    print(f"Error: Image {image_url} not found in votes")
 
     
-    def run(self):
+    def run(self) -> None:
+
+        if self.primary:
+            await_thread = threading.Thread(target=self.await_servers)
+            await_thread.start()
+            maintain_heartbeat_socket = threading.Thread(target=self.maintain_heartbeat_socket)
+            maintain_heartbeat_socket.start()
+
+        else:
+            rec_thread = threading.Thread(target=self.receive_updates)
+            rec_thread.start()
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                msg = {}
+                msg['host'] = self.host
+                msg['id'] = self.id
+                msg['receive_port'] = self.receive_port
+                msg['primary_port'] = self.internal_port
+                msg = json.dumps(msg)
+                s.connect((self.primary_host, self.primary_port))
+                s.sendall(msg.encode())
+                s.close()
+
+            time.sleep(2)
+            heartbeat_thread = threading.Thread(target=self.heartbeat)
+            heartbeat_thread.start()
+
         while True:
             client, addr = self.server.accept()
             print(f"New connection from {addr[0]}:{addr[1]}")
-            client_thread = threading.Thread(target=self.handle_client, args=(client,))
-            client_thread.start()
+
+            if self.primary:
+                client_thread = threading.Thread(target=self.handle_client, args=(client,))
+                client_thread.start()
+            else: 
+                print('Client connected to non-primary server!')
+                msg ='Tried to connect to a non-primary server. Failed.'
+                client.send(msg.encode())
+                client.close()
 
 if __name__ == "__main__":
-    server = Server("localhost", 8000)
+    parser = argparse.ArgumentParser(description="Server Connection")
+    parser.add_argument('--host', help='IPV4 Host', type=str)
+    parser.add_argument('--port', help='Port', type=int)
+    parser.add_argument('--pr', help='Primary/Secondary server', action='store_true')
+    parser.add_argument('--prhost', help='Primary Host', type=str)
+    parser.add_argument('--prport', help='Primary Host', type=int)
+    parser.add_argument('--id', help='Server ID', type=int)
+    args = parser.parse_args()
+    print(args)
+    server = Server(host=args.host, port=args.port, primary=args.pr, id=args.id, primary_host=args.prhost, primary_port=args.prport)
     server.run()
-
-
-    
