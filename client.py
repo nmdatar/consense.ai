91e876ab125356c162e75c3ce46bcd31b83a8712
Diana Yue <dandan@dhcp-10-250-218-141.harvard.edu>
2023-05-03 02:48:15 -0400

    added voting code

--- client.py
+++ client.py
@@ -3,6 +3,8 @@ import argparse
 import sys
 import select
 import threading
+import time
+from datetime import datetime  
 
 class Client:
     def __init__(self) -> None:
@@ -27,20 +29,40 @@ class Client:
 
     def send_request(self, request: str):
         self.clientsocket.send(request.encode())
+        return
 
+    def get_vote_and_timestamp(self, vote):
+        while vote not in ['Y', 'N']:
+            vote = input("Invalid vote. Please enter Y or N: ")
+        timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        return vote, timestamp
+    
     def receive_response(self):
         while True:
             response = self.clientsocket.recv(1024).decode()
-            print("Image URL: ", response)
-            return response
+            if "ImageURL" in response:
+                print(response)
+                vote = input(response)
+                vote, timestamp = self.get_vote_and_timestamp(vote)
+                vote_message = f"Vote: {vote}, Timestamp: {timestamp}"
+                # self.send_request(f"{vote}, {timestamp}")
+                self.send_request(vote)
+                break
+            else:
+                print(response)
+    
+    # def vote_process(self, request):
+    #     self.send_request(request)
+    #     self.receive_response()
+
 
     def run(self):
         listen_thread = threading.Thread(target=self.receive_response)
         listen_thread.start()
 
+    
         while True:
-            request = input("Enter a command (gen <parameter>, 'quit' to quit): ")
-            
+            request = input("Enter a command (gen <parameter>, 'quit' to quit): ")       
             # Exit the loop and close the client socket
             if request == "quit": 
                 self.send_request("quit")
@@ -49,8 +71,11 @@ class Client:
                 break
             elif self.check_error_command(request) == 0:
                 self.send_request(request)
+                self.receive_response()
+
             else:
                 print("Error: Invalid input. Please provide a valid command.")
+
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -7,6 +7,9 @@ import requests
 import ipfshttpclient
 import socket
 import threading
+import time
+from datetime import datetime  
+
 
 load_dotenv()
 
@@ -22,6 +25,10 @@ class Server:
         self.server.listen(5)
         self.clients = {}
         self.generate_available = 0
+        self.vote_count = {"Y":0, "N":0}
+        self.voting_end = False
+        self.voting_start_time = None
+        self.voting_duration = 30
         print(f"Server started on {host}:{port}")
     
     def generate_image(self, prompt: str) -> Dict:
@@ -33,15 +40,19 @@ class Server:
         return response
 
     def send_image(self, prompt, client):
-        print(self.generate_available)
+        print(f"Received prompt: {prompt}")
+        # print(self.generate_available)
         try:
-            self.generate_available = 1
+            # self.generate_available = 1
             image_response = self.generate_image(prompt)
             image_url = image_response['data'][0]['url']
             print("Generated image URL:", image_url)
-            self.send_to_all_clients(image_url)
+            self.send_to_all_clients(f"ImageURL: {image_url}\n Send Y/N to vote to mint this image:\n" )
             print(f"shared image to", [client.getpeername() for client in self.clients])
-        except Exception as e:
+            # vote_count_thread = threading.Thread(target=self.send_final_vote_count)
+            # vote_count_thread.start()
+            # self.reset_votes()
+        except Exception as e: 
             print("Exception:", e)
             self.send_to_all_clients(f"Error: {e}")
 
@@ -53,11 +64,30 @@ class Server:
                 print(f"Failed to send message to client {client.getpeername()}. Removing from clients list.")
                 client.close()
                 self.clients.pop(client)
+    
+    def process_vote(self, message: str, client) -> None:
+        vote = message[0]
+        # timestamp = message[2]
+        self.vote_count[vote] += 1
+        message = f"Received vote: {vote} from {client.getpeername()}"
+        self.send_to_all_clients(f"Received vote: {vote} from {client.getpeername()}")
+        # message = f"Received vote: {vote}, Timestamp: {timestamp} from {client.getpeername()}"
+        print(message)
+        return message 
+
+    
+    def reset_votes(self):
+        self.vote_count = {"Y": 0, "N": 0}
+        self.voting_end = False
+
+    def send_final_vote_count(self):
+        time.sleep(30)
+        self.send_to_all_clients(f"Final vote count: {self.vote_count}")
+        self.vote_count.clear()
 
     def handle_client(self, client):
         addr = client.getpeername()
         self.clients[client] = "active"
-        print("waiting for responses")
         while True:
             try:
                 request = client.recv(1024).decode().strip()
@@ -67,10 +97,19 @@ class Server:
                 self.clients.pop(client)
                 print(f"Client {client} disconnected")
             elif request[:3] == "gen":
-                self.clients[client] = "active"
                 prompt = request
                 self.send_image(prompt, client)
-                print(f"Received prompt: {prompt}")
+            elif request in ["Y", "N"]:
+                self.vote_count[request] += 1
+                vote_message = self.process_vote(request, client)
+                print(self.vote_count["Y"] , self.vote_count["N"], len(self.clients))
+                if (self.vote_count["Y"]//2 + self.vote_count["N"]//2) == len(self.clients):
+                    self.voting_end = True
+                    Y = self.vote_count["Y"]//2
+                    N = self.vote_count["N"]//2
+                    print(f"Voting ended. Results: 'Y:'{Y}, N:{N}")
+                    self.send_to_all_clients(f"Voting ended. Results: 'Y:'{Y}, N:{N}")
+                    self.reset_votes()            
 
     
     def run(self):
